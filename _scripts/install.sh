9ef0872b94221fd9edd20d418964f357c143f8bd
Petter <petter@maksimer.no>
2016-11-21 22:38:14 +0100

    Trying to get travis and autodeploy working

---
+++ _scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_b6b3065e2137_key -iv $encrypted_b6b3065e2137_iv -in deploy-key.enc -out deploy-key -d
+rm deploy-key.enc # Don't need it anymore
+chmod 600 deploy-key
