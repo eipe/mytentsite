10ab5a6b826f35fb95a9937c85510876100b89c3
Petter <petter@maksimer.no>
2016-11-22 22:53:53 +0100

    Travis deployment test #5

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -5,3 +5,4 @@ set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_b6b3065e2137_key -iv $encrypted_b6b3065e2137_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
