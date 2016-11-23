6872fbc9ec7da9be6a98a55fc9dc10b063037e4e
Eivind Røe <eivindroe@gmail.com>
2016-11-24 00:34:02 +0100

    #21 Added controller for social login authentication
    Controller takes care of redirect to auth provider and response from auth provider which on success will create a user and record the social login.

---
+++ app/Http/Controllers/Auth/SocialController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\SocialLogin;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Response;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @param string $provider
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        if(!Config::get('services.' . $provider))
+        {
+            die('Provider ' . $provider . ' is not supported');
+        }
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @param string $provider
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        // Check that provider exists as a service
+        if(!Config::get('services.' . $provider))
+        {
+            die('Provider ' . $provider . ' is not supported');
+        }
+
+        if(Input::get('denied') != '')
+        {
+            die('You did not allow us to connect with your social profile on ' . $provider);
+        }
+
+        $user = Socialite::driver($provider)->stateless()->user();
+
+        $socialUser = null;
+
+        //Check is this email present
+        $userCheck = User::where('email', '=', $user->email)->first();
+
+        if(!empty($userCheck))
+        {
+            $socialUser = $userCheck;
+        }
+        else
+        {
+            $sameSocialId = SocialLogin::where('social_id', '=', $user->id)
+                ->where('provider', '=', $provider)
+                ->first();
+
+            if(!empty($sameSocialId))
+            {
+                $socialUser = $sameSocialId->user;
+            }
+            else
+            {
+                // There is no combination of this social id and provider, so we create new one
+                $newSocialUser = new User;
+                $newSocialUser->email = $user->email;
+                $newSocialUser->name = $user->name;
+                $newSocialUser->password = bcrypt(str_random(16));
+                $newSocialUser->remember_token = str_random(64);
+                $newSocialUser->save();
+
+                $socialData = new SocialLogin;
+                $socialData->social_id = $user->id;
+                $socialData->provider= $provider;
+                $newSocialUser->socialLogin()->save($socialData);
+                $socialUser = $newSocialUser;
+            }
+        }
+
+        Auth::login($socialUser, true);
+        return Redirect::to('/');
+    }
+
+}
