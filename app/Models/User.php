21e814205a70e0367be5ff76bdf17b1194cdf3c1
Eivind Røe <eivindroe@gmail.com>
2016-11-24 00:32:12 +0100

    #21 Mapped user to social login
    A user can have many social login entires

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function socialLogin()
+    {
+        return $this->hasMany('App\Models\SocialLogin');
+    }
 }
