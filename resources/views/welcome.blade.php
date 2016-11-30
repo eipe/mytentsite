8244290b89858b4ba7b8204a5641441055f74335
Eivind Røe <eivindroe@gmail.com>
2016-11-30 17:03:28 +0100

    Changed title to lowercase mytentsite
    We have made a decision, lowercase m in mytentsite.
    This should be reflected overall

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Mytentsite - a collaborative tent collective</title>
+        <title>mytentsite - a collaborative tent collective</title>
         <link href="{{ asset('/css/vendor.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('/css/app_normalize.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
