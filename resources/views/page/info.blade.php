08bd1455d1c7be7663ab14ac03ce94d4b805acf6
Eivind Røe <eivindroe@gmail.com>
2016-12-05 19:55:45 +0100

    Adjusted info page so it wraps better on all screen resolutions

--- resources/views/page/info.blade.php
+++ resources/views/page/info.blade.php
@@ -1,29 +1,32 @@
 <div class="row"><br /></div>
 <div class="row">
-    <div class="medium-6 large-centered columns text-center">
-        <h4>The story and vision of mytentsite</h4>
-        <hr />
-        <br />
+    <div class="large-8 large-centered columns text-center">
+        <div class="row">
+            <div class="column">
+                <br>
+                <h4>The story and vision of mytentsite</h4>
+                <hr>
+                <br>
+                <p>As we have focused on developing the story has not yet been written in words.</p>
+                <p>If you have the time and desire to help us, we would appreciate that a lot, and we think
+                    this project deserves a good description. So please, do not hesitate to contact us if you
+                    want to make your contribution to this project.</p>
+            </div>
+        </div>
+        <hr>
         <div class="row">
             <div class="large-4 columns">
-                <i class="fa fa-5x fa-picture-o"></i><br />
+                <i class="fa fa-5x fa-picture-o" style="font-size: 120px;"></i><br>
                 Photo
             </div>
             <div class="large-4 columns">
-                <i class="fa fa-5x fa-map-marker"></i><br />
+                <i class="fa fa-5x fa-map-marker" style="font-size: 120px;"></i><br>
                 Location
             </div>
             <div class="large-4 columns">
-                <i class="fa fa-5x fa-font"></i><br />
+                <i class="fa fa-5x fa-font" style="font-size: 120px;"></i><br>
                 Caption
             </div>
         </div>
-        <br />
-        <hr />
-        <br />
-        <p>As we have focused on developing the story has not yet been written in words.</p>
-        <p>If you have the time and desire to help us, we would appreciate that a lot, and we think
-            this project deserves a good description. So please, do not hesitate to contact us if you
-            want to make your contribution to this project.</p>
     </div>
 </div>
