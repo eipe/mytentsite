b6490df33d6bcfcf68a8dd572b2656fb459004e9
Eivind Røe <eivindroe@gmail.com>
2016-12-05 22:28:12 +0100

    Adjusted html so it is valid
    Also adjusted column classes to match the size of container

--- resources/views/page/footer.blade.php
+++ resources/views/page/footer.blade.php
@@ -4,17 +4,12 @@
         <div class="row">
             <div class="large-4 columns">
                 <h4>mytentsite</h4>
-                <small>
-                    <p>We use cookies to give you the best possible user experience
-                        in accordance with Law on Electronic Communications (ekomloven).</p>
-                    <p>With continued use of our service, we assume that you agree to our use of cookies.<br>
-
-                    </P>
-                </small>
+                    <p><small>We use cookies to give you the best possible user experience
+                            in accordance with Law on Electronic Communications (ekomloven).</small></p>
+                    <p><small>With continued use of our service, we assume that you agree to our use of cookies.<br>
+                        </small></p>
             </div>
-            <div class="large-8 columns">
-
-            </div>
+            <div class="large-4 columns"></div>
         </div>
         <br>
     </div>
