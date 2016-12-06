a9774b36efdc7bc8f9bc324a3f758127df92b003
Eivind Røe <eivindroe@gmail.com>
2016-12-06 23:07:39 +0100

    Wrote some more about the page.
    Improved layout so it wraps and looks better.
    Obs: Photo is still missing, but will be added

--- resources/views/page/info.blade.php
+++ resources/views/page/info.blade.php
@@ -1,33 +1,53 @@
 <div class="row"><br /></div>
 <div class="row">
-    <div class="large-8 large-centered columns text-center">
-        <div class="row">
-            <div class="column">
-                <br>
-                <h4>The story and vision of mytentsite</h4>
-                <hr>
-                <br>
-                <p>As we have focused on developing the story has not yet been written in words.</p>
-                <p>If you have the time and desire to help us, we would appreciate that a lot, and we think
-                    this project deserves a good description. So please, do not hesitate to contact us if you
-                    want to make your contribution to this project.</p>
+    <div class="columns large-centered text-center">
+        <h4>mytentsite</h4>
+        <img src="/storage/photos/500.png" />
+    </div>
+</div>
+<br><br>
+<div class="row background-light-gray">
+    <div class="large-8 columns large-centered">
+        <br><br>
+        <div class="row medium-unstack text-left">
+            <div class="large-4 columns">
+                <h4><i class="fa fa-2x fa-picture-o"></i> Photo</h4>
+                <p>Select a photo of tent site you want to share with others and preferably a photo that
+                    brings out what you think is great with just the place.</p>
+                <p><em>A photo is often worth more than a thousand words.</em></p>
+            </div>
+            <div class="large-4 columns">
+                <h4><i class="fa fa-2x fa-map-marker"></i> Location</h4>
+                <p>Location, location, location. It is not important whether the tent site is located in top of a
+                    mountain or besides a lake in the middle of the forrest.
+                    But to make it possible to locate the tent site and serve your tent site on the map, we need
+                    the GPS information.
+                </p>
+            </div>
+            <div class="large-4 columns">
+                <h4><i class="fa fa-2x fa-font"></i> Caption</h4>
+                <p>Tell other campers why this is a preferable tent site, and give some extra information you think
+                    can help them make a decision of where to go camping. Be short and concise, we can see your photo.
+                </p>
             </div>
         </div>
-        <hr>
-        <div class="row">
-            <div class="large-4 columns">
-                <i class="fa fa-5x fa-picture-o"></i><br>
-                Photo
-            </div>
-            <div class="large-4 columns">
-                <i class="fa fa-5x fa-map-marker"></i><br>
-                Location
-            </div>
-            <div class="large-4 columns">
-                <i class="fa fa-5x fa-font"></i><br>
-                Caption
-            </div>
+        <br><br>
+    </div>
+</div>
+<div class="row">
+    <div class="large-8 columns large-centered">
+        <br><br>
+        <div class="small-2 columns">
+            <i class="fa fa-5x fa-comment"></i>
         </div>
+        <div class="small-10 columns text-left">
+            <h4>The story and vision of mytentsite</h4>
+            <p>As we have focused on developing the story has not yet been written in words.</p>
+            <p>If you have the time and desire to help us, we would appreciate that a lot, and we think
+                this project deserves a good description. So please, do not hesitate to contact us if you
+                want to make your contribution to this project.</p>
+        </div>
+        <br><br>
     </div>
 </div>
 <br><br>
