8417fe35b12db26670c82b6d34cd0fe182a3eb3c
Eivind Røe <eivindroe@gmail.com>
2016-12-06 23:06:52 +0100

    Fixed styling of footer and improved the text
    Added information about social media channels

--- resources/views/page/footer.blade.php
+++ resources/views/page/footer.blade.php
@@ -1,17 +1,23 @@
 <div class="row background-light-gray">
-    <div class="large-8 large-centered columns">
-        <br>
-        <div class="row">
-            <div class="large-4 columns">
-                <h4>mytentsite</h4>
-                    <p><small>We use cookies to give you the best possible user experience
-                            in accordance with Law on Electronic Communications (ekomloven).</small></p>
-                    <p><small>With continued use of our service, we assume that you agree to our use of cookies.<br>
-                        </small></p>
+    <div class="large-8 large-centered columns background-light-gray">
+        <div class="row medium-unstack">
+            <div class="medium-6 columns">
+                <br>
+                <h5>mytentsite</h5>
+                <p><small>We use cookies to give you the best possible user experience
+                        in accordance with Law on Electronic Communications (ekomloven).</small></p>
+                <p><small>With continued use of our service, we assume that you agree to our use of cookies.<br>
+                    </small></p>
             </div>
-            <div class="large-4 columns"></div>
+            <div class="medium-6 columns">
+                <br>
+                <p><i class="fa fa-3x fa-instagram"></i> @mytentsite&nbsp;&nbsp;<i class="fa fa-3x fa-facebook-square"></i> mytentsite</p>
+                <p><small>Spread the word and inspire others to share their favourite tent sites!<br>
+                        We encourage you to bring this message further on, in hope of a growing database of
+                    tent sites throughout the world. We will promote tent sites through this instagram account on
+                    request, to spread the word and inspire to share.</small></p>
+            </div>
         </div>
-        <br>
     </div>
 </div>
 <div class="is-hidden" id="api_token">{{Auth::user()->getAttribute('api_token')}}</div>
