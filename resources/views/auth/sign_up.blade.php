7db845bed77671449293819287c2fe70e6cff2ca
Eivind Røe <eivindroe@gmail.com>
2016-12-08 19:34:25 +0100

    Adjusted content so it scales better on small screens

--- resources/views/auth/sign_up.blade.php
+++ resources/views/auth/sign_up.blade.php
@@ -1,6 +1,6 @@
 <div class="row"><br></div>
 <div class="row">
-    <div class="medium-6 large-centered columns text-center">
+    <div class="columns large-centered text-center">
         <h4>Please sign up</h4>
         <hr>
         <p>We want to give you control over your contributions, and to make that possible we need
