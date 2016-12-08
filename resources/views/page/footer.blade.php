a04febb8609a9187a361dfc0b1d50f7385c1ca84
Eivind Røe <eivindroe@gmail.com>
2016-12-08 19:33:43 +0100

    Adjusted footer so it looks better

--- resources/views/page/footer.blade.php
+++ resources/views/page/footer.blade.php
@@ -1,22 +1,23 @@
 <div class="row background-light-gray">
     <div class="large-8 large-centered columns background-light-gray">
+        <br><br>
         <div class="row medium-unstack">
-            <div class="medium-6 columns">
-                <br>
-                <h5>mytentsite</h5>
+            <div class="medium-5 columns">
+                <h5>Information about our use of cookies</h5>
                 <p><small>We use cookies to give you the best possible user experience
                         in accordance with Law on Electronic Communications (ekomloven).</small></p>
                 <p><small>With continued use of our service, we assume that you agree to our use of cookies.<br>
                     </small></p>
             </div>
+            <div class="medium-1 columns"></div>
             <div class="medium-6 columns">
-                <br>
-                <p><i class="fa fa-3x fa-instagram"></i> @mytentsite&nbsp;&nbsp;<i class="fa fa-3x fa-facebook-square"></i> mytentsite</p>
-                <p><small>Spread the word and inspire others to share their favourite tent sites!<br>
-                        We encourage you to bring this message further on, in hope of a growing database of
+                <h5>Spread the word and inspire others<br><small>- to share their favourite tent sites!</small></h5>
+                <p><small>We encourage you to bring this message further on, in hope of a growing database of
                     tent sites throughout the world. We will promote tent sites through this instagram account on
                     request, to spread the word and inspire to share.</small></p>
+                <p><i class="fa fa-2x fa-instagram"></i> <i class="fa fa-2x fa-facebook-square"></i></p>
             </div>
         </div>
+        <br><br>
     </div>
 </div>
