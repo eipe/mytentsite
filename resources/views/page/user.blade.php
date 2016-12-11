c7a77b97e0134543255459df7b78a05b00600bf5
Eivind Røe <eivindroe@gmail.com>
2016-12-11 14:42:30 +0100

    Added container of contributions on user page
    This makes it possible for user to review own contributions

--- resources/views/page/user.blade.php
+++ resources/views/page/user.blade.php
@@ -8,14 +8,41 @@
             <p>You signed up through
             {{ ucfirst(trans(Auth::user()->socialLogin()->first()->provider)) }},<br>
             and the last authentication was {{ Auth::user()->socialLogin()->first()->updated_at }}.</p>
+                <a href="/auth/sign_out" class="button">Sign out</a>
             @endif
         </div>
     </div>
-    <div class="row">
-        <div class="large-8 large-centered columns text-center">
-            <a href="/auth/sign_out" class="button">Sign out</a>
+    @if(Auth::user()->tentSites())
+        <div class="row background-light-gray">
+            <div class="large-8 large-centered columns text-center">
+                <br>
+                <h5><a data-toggle="user-contributions" title="Click to toggle view of contributions">
+                        Your contributions ({{ Auth::user()->tentSites()->count() }})</a></h5>
+                <br>
+            </div>
+            <div id="user-contributions" class="large-8 large-centered is-hidden" data-toggler=".is-hidden">
+            @foreach(Auth::user()->tentSites()->getResults() as $tentSite)
+                <div class="medium-4 column">
+                    <img src="/storage/photos/tentsites/{{ $tentSite->img_location }}" />
+                    <p>
+                        <small>
+                            {{ $tentSite->created_at }} |
+                            <i class="fa fa-thumbs-up"></i> {{ $tentSite->likes }} |
+                            @if($tentSite->approved)
+                                <i class="fa fa-check"></i> Approved
+                            @else
+                                <i class="fa fa-circle"></i> Waiting for approval
+                            @endif
+                        </small>
+                    </p>
+                    <p><small>{{ $tentSite->caption }}</small></p>
+                </div>
+            @endforeach
+                <br><br>
+            </div>
         </div>
-    </div>
+        <br><br>
+    @endif
     @include('page.footer')
 @else
     @include('auth.sign_up')
