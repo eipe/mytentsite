76c8cdb68a9304e0ce4613d3847d59ed2aabd82b
Eivind Røe <eivindroe@gmail.com>
2016-12-17 12:22:18 +0100

    Removed pre loaded containers as they are not useful
    (cherry picked from commit 14d1684)

--- resources/views/page/wall.blade.php
+++ resources/views/page/wall.blade.php
@@ -8,18 +8,7 @@
     <p id="wall-fullscreen-caption"></p>
     <p id="wall-fullscreen-reported"></p>
 </div>
-<div id="wall-photos">
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-    <div class="wall-photo-container"></div>
-</div>
+<div id="wall-photos"></div>
 <br />
 <div class="row">
     <button class="button float-center is-hidden" id="wall-load-more">Load more tent site photos</button>
