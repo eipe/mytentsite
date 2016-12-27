baf2c078fcc023c48ad82593f71ff933e2a4d303
Eivind Røe <eivindroe@gmail.com>
2016-12-27 21:45:39 +0100

    Added extend of layout as these blades is accessed directly, not through welcome blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,71 @@
-<div class="row"><br/></div>
-<div class="row">
-    <div class="columns large-centered text-center">
-        <h4>Create a new account</h4>
-        <form role="form" method="POST" action="{{ url('/register') }}">
-            {{ csrf_field() }}
+@extends('layouts.app')
+@section('content')
+<div class="page page-allow-overflow">
+    <div class="row"><br/></div>
+    <div class="row">
+        <div class="columns large-centered text-center">
+            <h4>Create a new account</h4>
+            <form role="form" method="POST" action="{{ url('/register') }}">
+                {{ csrf_field() }}
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-left">
-                    <label for="name">Name</label>
-                    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-left">
+                        <label for="name">Name</label>
+                        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
 
-                    @if ($errors->has('name'))
-                        <span class="round alert label">{{ $errors->first('name') }}</span>
-                    @endif
+                        @if ($errors->has('name'))
+                            <span class="round alert label">{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-left">
-                    <label for="email">E-Mail Address</label>
-                    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-                    @if ($errors->has('email'))
-                        <span class="round alert label">{{ $errors->first('email') }}</span>
-                    @endif
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-left">
+                        <label for="email">E-Mail Address</label>
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+                        @if ($errors->has('email'))
+                            <span class="round alert label">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-left">
-                    <label for="password">Password</label>
-                    <input id="password" type="password" name="password" required>
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-left">
+                        <label for="password">Password</label>
+                        <input id="password" type="password" name="password" required>
 
-                    @if ($errors->has('password'))
-                        <span class="round alert label">{{ $errors->first('password') }}</span>
-                    @endif
+                        @if ($errors->has('password'))
+                            <span class="round alert label">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-left">
-                    <label for="password-confirm">Confirm Password</label>
-                    <input id="password-confirm" type="password" name="password_confirmation" required>
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-left">
+                        <label for="password-confirm">Confirm Password</label>
+                        <input id="password-confirm" type="password" name="password_confirmation" required>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-right">
-                    <button type="submit" class="button expanded">
-                        Create a new account
-                    </button>
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-right">
+                        <button type="submit" class="button expanded">
+                            Create a new account
+                        </button>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="medium-5 columns large-centered text-right">
-                    <a href="/auth/facebook" class="button">
-                        <i class="fa fa-facebook"></i>&nbsp;&nbsp;Sign up with Facebook
-                    </a>
+                <div class="row">
+                    <div class="medium-5 columns large-centered text-right">
+                        <a href="/auth/facebook" class="button">
+                            <i class="fa fa-facebook"></i>&nbsp;&nbsp;Sign up with Facebook
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-        </form>
+            </form>
+        </div>
     </div>
+    @include('page.footer')
 </div>
-@include('page.footer')
+@endsection
