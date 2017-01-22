9e2831b05c2d75bcb9545fa02d765e4cc0563133
Petter <petter@maksimer.no>
2017-01-22 14:01:41 +0100

    404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('welcome')
+@section('content')
+    <div class="page page-allow-overflow">
+        <div class="row">
+            <div class="columns large-4 large-centered text-center" style="margin-top: 40px;">
+                <img src="/images/mytentsite.svg" />
+                <h4>404 - Not found</h4>
+                <p>We could not find the page you were looking for!</p>
+            </div>
+        </div>
+    </div>
+@endsection
