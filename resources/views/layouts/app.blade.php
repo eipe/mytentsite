9111349a6a292fc7c96d5741dbb6546bdb8c2906
Eivind Røe <eivindroe@gmail.com>
2017-02-19 13:51:58 +0100

    Moved content form welcome blade to app blade as we dont need this separation anymore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,25 @@
 </head>
 <body>
 <div id="app">
-    @section('navigation')
-    @show
+    <nav class="top-bar">
+        @if(Config::get('app.env') === 'staging')
+            <div style="position: absolute; top: 10px; color: #e00;">
+                &nbsp;&nbsp;<small><strong>BETA</strong></small>
+            </div>
+        @endif
+        <ul id="menu" class="menu align-center">
+            <li><router-link to="/share"><i class="fa fa-camera"></i></router-link></li>
+            <li><router-link to="/map"><i class="fa fa-map-o"></i></router-link></li>
+            <li><router-link to="/wall"><i class="fa fa-th"></i></router-link></li>
+            <li><router-link to="/info"><i class="fa fa-info"></i></router-link></li>
+            <li><router-link to="/user"><i class="fa fa-user-o"></i></router-link></li>
+            <li><a href="{{ url('/admin/#admin') }}"><i class="fa fa-unlock-alt"></i></a></li>
+        </ul>
+    </nav>
     <div id="content">
-        @section('content')
-        @show
+        <keep-alive>
+            <router-view :staging="{{ (Config::get('app.env') === 'staging') }}"></router-view>
+        </keep-alive>
     </div>
     @if(Auth::check())
         <div class="is-hidden" id="api_token">{{Auth::user()->getAttribute('api_token')}}</div>
