49f4b34754638e881b99cd477766d906adc4e8ab
Eivind Røe <eivindroe@gmail.com>
2017-02-22 19:51:51 +0100

    Reintroduced columns for components that should have small centered content

--- resources/assets/js/pages/Login.vue
+++ resources/assets/js/pages/Login.vue
@@ -1,59 +1,61 @@
 <template>
     <div class="page page-allow-overflow">
         <section class="section">
-            <div class="content container">
-                <h1>Login</h1>
-                    <!--<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">-->
-                <form action="/login" method="POST">
-                    <!--{{ csrf_field() }}-->
-                    <label class="label" for="email">E-Mail Address</label>
-                    <p class="control">
-                    <!--<input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>-->
-                        <input id="email" type="email" name="email" value="" class="input" required autofocus>
-                    </p>
-                    <!--@if ($errors->has('email'))-->
-                    <!--<span class="round alert label">{{ $errors->first('email') }}</span>-->
-                    <!--@endif-->
+            <div class="content columns">
+                <div class="column is-half is-offset-one-quarter">
+                    <h1>Login</h1>
+                        <!--<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">-->
+                    <form action="/login" method="POST">
+                        <!--{{ csrf_field() }}-->
+                        <label class="label" for="email">E-Mail Address</label>
+                        <p class="control">
+                        <!--<input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>-->
+                            <input id="email" type="email" name="email" value="" class="input" required autofocus>
+                        </p>
+                        <!--@if ($errors->has('email'))-->
+                        <!--<span class="round alert label">{{ $errors->first('email') }}</span>-->
+                        <!--@endif-->
 
-                    <label class="label" for="password">Password</label>
-                    <p class="control">
-                        <input id="password" type="password" name="password" class="input" required>
-                    </p>
-                    <!--@if ($errors->has('password'))-->
-                    <!--<span class="round alert label">{{ $errors->first('password') }}</span>-->
-                    <!--@endif-->
+                        <label class="label" for="password">Password</label>
+                        <p class="control">
+                            <input id="password" type="password" name="password" class="input" required>
+                        </p>
+                        <!--@if ($errors->has('password'))-->
+                        <!--<span class="round alert label">{{ $errors->first('password') }}</span>-->
+                        <!--@endif-->
 
-                    <p class="control">
-                        <label class="checkbox">
-                            <input type="checkbox" name="remember" class="checkbox"> Remember me
-                        </label>
-                    </p>
+                        <p class="control">
+                            <label class="checkbox">
+                                <input type="checkbox" name="remember" class="checkbox"> Remember me
+                            </label>
+                        </p>
 
-                    <div class="control is-grouped">
-                        <p class="control">
-                            <button type="submit" class="button is-primary">Login</button>
-                        </p>
-                        <p class="control">
-                            <router-link to="/password/reset" tag="button" class="button is-link">
-                                Forgot your password?
-                            </router-link>
-                        </p>
-                        <p class="control">
-                            <router-link to="/register" tag="button" class="button is-link">
-                                Don't have an account?
-                            </router-link>
-                        </p>
-                    </div>
-                    <hr>
-                    <h2 class="is-small">Or use one of your existing accounts</h2>
-                    <div class="control is-grouped">
-                        <p class="control">
-                            <a class="button is-info" href="/auth/facebook">
-                                <i class="fa fa-facebook"></i>&nbsp;&nbsp;Facebook
-                            </a>
-                        </p>
-                    </div>
-                </form>
+                        <div class="control is-grouped">
+                            <p class="control">
+                                <button type="submit" class="button is-primary">Login</button>
+                            </p>
+                            <p class="control">
+                                <router-link to="/password/reset" tag="button" class="button is-link">
+                                    Forgot your password?
+                                </router-link>
+                            </p>
+                            <p class="control">
+                                <router-link to="/register" tag="button" class="button is-link">
+                                    Don't have an account?
+                                </router-link>
+                            </p>
+                        </div>
+                        <hr>
+                        <h2 class="is-small">Or use one of your existing accounts</h2>
+                        <div class="control is-grouped">
+                            <p class="control">
+                                <a class="button is-info" href="/auth/facebook">
+                                    <i class="fa fa-facebook"></i>&nbsp;&nbsp;Facebook
+                                </a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </section>
     </div>
--- resources/assets/js/pages/PasswordReset.vue
+++ resources/assets/js/pages/PasswordReset.vue
@@ -1,36 +1,38 @@
 <template>
     <div class="page page-allow-overflow">
         <section class="section">
-            <div class="content container">
-                <h1>Reset password</h1>
-                <!--@if (session('status'))-->
-                <!--<div class="alert alert-success">-->
-                <!--{{ session('status') }}-->
-                <!--</div>-->
-                <!--@endif-->
-                <!--<div class="columns large-4 large-centered text-left form-group{{ $errors->has('email') ? ' has-error' : '' }}">-->
-                <form method="POST" action="/password/email">
-                    <!--{{ csrf_field() }}-->
-                    <label for="email" class="label">E-mail address</label>
-                    <!--<label for="email" class="col-md-4 control-label">E-mail address</label>-->
-                    <p class="controls">
-                        <input id="email" type="email" class="input" name="email" value="" required>
-                    </p>
-                        <!--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>-->
-                        <!--@if ($errors->has('email'))-->
-                        <!--<span class="help-block">-->
-                        <!--<strong>{{ $errors->first('email') }}</strong>-->
-                        <!--</span>-->
-                        <!--@endif-->
-                    <div class="control is-grouped">
-                        <p class="control">
-                            <button type="submit" class="button is-primary">Send password reset link</button>
+            <div class="content columns">
+                <div class="column is-half is-offset-one-quarter">
+                    <h1>Reset password</h1>
+                    <!--@if (session('status'))-->
+                    <!--<div class="alert alert-success">-->
+                    <!--{{ session('status') }}-->
+                    <!--</div>-->
+                    <!--@endif-->
+                    <!--<div class="columns large-4 large-centered text-left form-group{{ $errors->has('email') ? ' has-error' : '' }}">-->
+                    <form method="POST" action="/password/email">
+                        <!--{{ csrf_field() }}-->
+                        <label for="email" class="label">E-mail address</label>
+                        <!--<label for="email" class="col-md-4 control-label">E-mail address</label>-->
+                        <p class="controls">
+                            <input id="email" type="email" class="input" name="email" value="" required>
                         </p>
-                        <p class="control">
-                            <button @click="back" class="button is-link">Back</button>
-                        </p>
-                    </div>
-                </form>
+                            <!--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>-->
+                            <!--@if ($errors->has('email'))-->
+                            <!--<span class="help-block">-->
+                            <!--<strong>{{ $errors->first('email') }}</strong>-->
+                            <!--</span>-->
+                            <!--@endif-->
+                        <div class="control is-grouped">
+                            <p class="control">
+                                <button type="submit" class="button is-primary">Send password reset link</button>
+                            </p>
+                            <p class="control">
+                                <button @click="back" class="button is-link">Back</button>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </section>
     </div>
