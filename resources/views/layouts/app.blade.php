a9b5ceb358227b23d718567eaec8d445862cb0f5
Eivind Røe <eivindroe@gmail.com>
2017-02-24 07:13:50 +0100

    Fixed navigation layout
    Removing height override as it looks better with normal height
    Moved nav-item class to li element to make it use the full nav height
    Replaced beta class with red style color as class only works on buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,15 +17,15 @@
 <div id="app">
     <nav class="nav">
         @if(Config::get('app.env') === 'staging')
-            <div class="is-pulled-left is-danger is-bold">BETA</div>
+            <div class="is-pulled-left is-bold nav-item"><span style="color: #f00">BETA</span></div>
         @endif
         <ul class="nav-center">
-            <li><router-link to="/share" class="nav-item"><i class="fa fa-camera"></i></router-link></li>
-            <li><router-link to="/map" class="nav-item"><i class="fa fa-map-o"></i></router-link></li>
-            <li><router-link to="/wall" class="nav-item"><i class="fa fa-th"></i></router-link></li>
-            <li><router-link to="/info" class="nav-item"><i class="fa fa-info"></i></router-link></li>
-            <li><router-link to="/user" class="nav-item"><i class="fa fa-user-o"></i></router-link></li>
-            <li><router-link to="/admin" class="nav-item"><i class="fa fa-unlock-alt"></i></router-link></li>
+            <li class="nav-item"><router-link to="/share"><i class="fa fa-camera"></i></router-link></li>
+            <li class="nav-item"><router-link to="/map"><i class="fa fa-map-o"></i></router-link></li>
+            <li class="nav-item"><router-link to="/wall"><i class="fa fa-th"></i></router-link></li>
+            <li class="nav-item"><router-link to="/info"><i class="fa fa-info"></i></router-link></li>
+            <li class="nav-item"><router-link to="/user"><i class="fa fa-user-o"></i></router-link></li>
+            <li class="nav-item"><router-link to="/admin"><i class="fa fa-unlock-alt"></i></router-link></li>
         </ul>
     </nav>
     <div id="content">
