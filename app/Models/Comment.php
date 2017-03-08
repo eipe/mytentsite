742ce1e08efd49fb6073663d0590d1dc9f45f96c
Eivind Røe <eivindroe@gmail.com>
2017-03-08 22:07:57 +0100

    Convert user id to user name to be displayed on comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -17,5 +17,25 @@ class Comment extends Model
         'comment'
     ];
 
+    public function userId() {
+        return $this->belongsTo('App\Models\User', 'id');
+    }
+
+    /**
+     * Get the user's name.
+     *
+     * @param  string  $id
+     * @return string
+     */
+    public function getUserIdAttribute($id)
+    {
+        $user = \DB::table('users')->where('id', $id)->first();
+        if(is_object($user)) {
+            return $user->name;
+        }
+        return $id;
+
+    }
+
 
 }
