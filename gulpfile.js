5d26e6d873de69429c705fef2086e598802df8c0
Eivind Røe <eivindroe@gmail.com>
2017-03-19 10:47:38 +0100

    Reduced need of bower - can we get rid of that dependency?

--- gulpfile.js
+++ gulpfile.js
@@ -23,15 +23,15 @@ elixir(function(mix) {
 
     mix.styles('app_normalize.css', 'public/css/app_normalize.css')
         .styles([
-            config.bowerDir + '/font-awesome/css/font-awesome.css',
+            config.nodeDir + '/font-awesome/css/font-awesome.css',
             config.nodeDir + '/animate.css/animate.css'
         ], 'public/css/core-dep.css')
         .styles([
-            config.bowerDir + '/leaflet/dist/leaflet.css',
-            config.bowerDir + '/leaflet.markercluster/dist/MarkerCluster.css',
-            config.bowerDir + '/leaflet.markercluster/dist/MarkerCluster.Default.css',
+            config.nodeDir + '/leaflet/dist/leaflet.css',
+            config.nodeDir + '/leaflet.markercluster/dist/MarkerCluster.css',
+            config.nodeDir + '/leaflet.markercluster/dist/MarkerCluster.Default.css',
             config.bowerDir + '/Leaflet.Photo/Leaflet.Photo.css',
-            config.bowerDir + '/Leaflet.EasyButton/src/easy-button.css',
+            config.nodeDir + '/leaflet-easybutton/src/easy-button.css',
         ], 'public/css/map-dep.css');
 
     mix.webpack('app.js', 'public/js/app.js')
@@ -43,15 +43,15 @@ elixir(function(mix) {
             config.nodeDir + '/vuex/dist/vuex.js'
         ], 'public/js/core-dep.js')
         .scripts([
-            config.bowerDir + '/leaflet/dist/leaflet.js',
-            config.bowerDir + '/leaflet.markercluster/dist/leaflet.markercluster.js',
+            config.nodeDir + '/leaflet/dist/leaflet.js',
+            config.nodeDir + '/leaflet.markercluster/dist/leaflet.markercluster.js',
             config.bowerDir + '/Leaflet.Photo/Leaflet.Photo.js',
-            config.bowerDir + '/Leaflet.EasyButton/src/easy-button.js',
+            config.nodeDir + '/leaflet-easybutton/src/easy-button.js',
         ], 'public/js/map-dep.js')
         .scripts([
-            config.bowerDir + '/jquery/dist/jquery.js',
-            config.bowerDir + '/exif-js/exif.js',
-            config.bowerDir + '/cropit/dist/jquery.cropit.js',
+            config.nodeDir + '/jquery/dist/jquery.js',
+            config.nodeDir + '/exif-js/exif.js',
+            config.nodeDir + '/cropit/dist/jquery.cropit.js',
         ], 'public/js/share-dep.js');
 });
 
