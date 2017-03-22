348eb9fd737ce9ca2ae29e38692fb510c129c3ec
Eivind Røe <eivindroe@gmail.com>
2017-03-22 23:41:03 +0100

    Refactored form so it adopts to bulma and async saving
    Requires changes in api routes to actually register user and receive token

--- resources/assets/js/pages/Register.vue
+++ resources/assets/js/pages/Register.vue
@@ -4,58 +4,54 @@
             <div class="content columns">
                 <div class="column is-half is-offset-one-quarter">
                     <h1>Create a new account</h1>
-                    <form method="POST" action="/register">
-                        <!--{{ csrf_field() }}-->
-
-                        <label for="name" class="label">Name</label>
+                    <form method="post" @submit.prevent="submitForm">
                         <p class="controls">
-                            <input id="name" type="text" name="name" value="" class="input"
-                                   placeholder="Name" required autofocus>
-                            <!--<input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>-->
-                            <!--@if ($errors->has('name'))-->
-                            <!--<span class="round alert label">{{ $errors->first('name') }}</span>-->
-                            <!--@endif-->
+                            <input id="name" type="text" name="name" class="input"
+                                   placeholder="Name"
+                                   v-model="info.name" required autofocus>
+                            <span class="round alert label" v-if="errors.name">{{ errors.name }}</span>
                         </p>
-
-                        <label for="email" class="label">E-mail address</label>
                         <p class="controls">
-                            <input id="email" type="email" name="email" value="" class="input"
-                                   placeholder="E-mail address" required>
-                            <!--<input id="email" type="email" name="email" value="{{ old('email') }}" required>-->
-                            <!--@if ($errors->has('email'))-->
-                            <!--<span class="round alert label">{{ $errors->first('email') }}</span>-->
-                            <!--@endif-->
+                            <input id="email" type="email" name="email" class="input"
+                                   placeholder="E-mail address"
+                                   v-model="info.email" required>
+                            <span class="round alert label" v-if="errors.email">{{ errors.email }}</span>
                         </p>
-
-                        <label for="password" class="label">Password</label>
                         <p class="controls">
                             <input id="password" type="password" name="password" class="input"
-                                   placeholder="Password" required>
-                            <!--@if ($errors->has('password'))-->
-                            <!--<span class="round alert label">{{ $errors->first('password') }}</span>-->
-                            <!--@endif-->
+                                   placeholder="Password"
+                                   v-model="info.password" required>
+                            <span class="round alert label" v-if="errors.password">{{ errors.password }}</span>
                         </p>
-
-                        <label for="password-confirm" class="label">Confirm password</label>
                         <p class="controls">
                             <input id="password-confirm" type="password"
-                                   name="password_confirmation" class="input" placeholder="Confirm password" required>
+                                   name="password_confirmation" class="input"
+                                   placeholder="Confirm password"
+                                   v-model="info.password_confirmation" required>
+                            <span class="round alert label" v-if="errors.password_confirmation">
+                                {{ errors.password_confirmation }}
+                            </span>
                         </p>
 
-                        <label for="subscribe" class="label">
-                            <input id="subscribe" type="checkbox"
-                                   name="subscribe" class="checkbox"> Subscribe to our mailinglist
-                        </label>
+                        <p class="controls">
+                            <label for="subscribe" class="label">
+                                <input id="subscribe" type="checkbox"
+                                       name="subscribe" class="checkbox"
+                                       v-model="info.subscribe"> Subscribe to our mailinglist
+                            </label>
+                        </p>
+
+                        <transition enter-active-class="animated shake" leave-active-class="animated fadeOut">
+                            <div class="notification is-danger" v-if="errors.message">
+                                <span class="delete" @click.prevent="errors.message = null"></span>
+                                {{ errors.message }}
+                            </div>
+                        </transition>
 
                         <div class="control is-grouped">
                             <p class="control">
-                                <button type="submit" class="button is-primary">Create a new account</button>
-                            </p>
-                            <p class="control">
-                                <a href="/auth/facebook" class="button is-info">
-                                    <span class="icon is-small"><i class="fa fa-facebook"></i></span>
-                                    <span>Sign up using Facebook</span>
-                                </a>
+                                <button type="submit" class="button is-primary"
+                                        v-bind:class="{ 'is-loading' : isPosting }">Create a new account</button>
                             </p>
                             <p class="control">
                                 <button @click="back" class="button is-link">Back</button>
@@ -72,11 +68,37 @@
         name: 'Register',
         data() {
             return {
+                isPosting: false,
+                errors: {},
+                info: {
+                    name: null,
+                    email: null,
+                    password: null,
+                    password_confirmation: null,
+                    subscribe: false
+                }
             }
         },
         methods: {
             back() {
                 this.$router.go(-1);
+            },
+            submitForm() {
+                let me = this;
+                me.isPosting = true;
+                me.errors = {};
+
+                axios.post('/register', me.info).then(function(success) {
+                    me.isPosting = false;
+                }).catch(function(error) {
+                    if(typeof error.response.data.error !== typeof undefined) {
+                        me.errors = error.response.data.error;
+                    }
+                    if(typeof error.message !== typeof undefined) {
+                        me.errors.message = error.message;
+                    }
+                    me.isPosting = false;
+                });
             }
         }
     }
