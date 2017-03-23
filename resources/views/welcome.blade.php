dcdd8210a7f5e6cd0fdf159616412dc137de34fa
Eivind Røe <eivindroe@gmail.com>
2017-03-23 23:48:23 +0100

    Fixed blade section bug
    Sections was removed, which caused problem with error blades that could not change content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.app')
 
 @section('navigation')
-<nav class="top-bar">
-    @if(Config::get('app.env') === 'staging')
-        <div style="position: absolute; top: 10px; color: #e00;">
-            &nbsp;&nbsp;<small><strong>BETA</strong></small>
-        </div>
-    @endif
-    <ul id="menu" class="menu align-center">
-        <li><router-link to="/share"><i class="fa fa-camera"></i></router-link></li>
-        <li><router-link to="/map"><i class="fa fa-map-o"></i></router-link></li>
-        <li><router-link to="/wall"><i class="fa fa-th"></i></router-link></li>
-        <li><router-link to="/info"><i class="fa fa-info"></i></router-link></li>
-        <li><router-link to="/user"><i class="fa fa-user-o"></i></router-link></li>
-        <li><router-link to="/admin"><i class="fa fa-unlock-alt"></i></router-link></li>
-    </ul>
-</nav>
+    <nav class="nav">
+        @if(Config::get('app.env') === 'staging')
+            <div class="is-pulled-left is-bold nav-item"><span style="color: #f00">BETA</span></div>
+        @endif
+        <ul class="nav-center">
+            <li class="nav-item"><router-link to="/share"><i class="fa fa-camera"></i></router-link></li>
+            <li class="nav-item"><router-link to="/map"><i class="fa fa-map-o"></i></router-link></li>
+            <li class="nav-item"><router-link to="/wall"><i class="fa fa-th"></i></router-link></li>
+            <li class="nav-item"><router-link to="/info"><i class="fa fa-info"></i></router-link></li>
+            <li class="nav-item"><router-link to="/user"><i class="fa fa-user-o"></i></router-link></li>
+            <li class="nav-item"><router-link to="/admin"><i class="fa fa-unlock-alt"></i></router-link></li>
+        </ul>
+    </nav>
 @endsection
 
 @section('content')
-    <keep-alive>
-            <router-view :staging="{{ (Config::get('app.env') === 'staging') }}"></router-view>
-    </keep-alive>
+    <div id="content">
+        <transition enter-active-class="animated fadeIn">
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
+        </transition>
+    </div>
+    <photo-gallery></photo-gallery>
 @endsection
