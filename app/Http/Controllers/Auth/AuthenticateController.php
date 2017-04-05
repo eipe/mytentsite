b874903da496874d71131bacadead431bf5e4a7a
Petter <petter@maksimer.no>
2017-04-05 23:58:53 +0200

    Authentiation from user, prepared for third part oauth

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -20,8 +20,6 @@ class AuthenticateController extends \App\Http\Controllers\Controller
 
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
-        // From facebook
-        //JWTAuth::fromUser($username);
 
         try {
             // attempt to verify the credentials and create a token for the user
@@ -32,7 +30,6 @@ class AuthenticateController extends \App\Http\Controllers\Controller
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-
         // all good so return the token
         return response()->json(compact('token'));
     }
@@ -64,4 +61,19 @@ class AuthenticateController extends \App\Http\Controllers\Controller
         return response()->json(compact('user'));
     }
 
+
+    public static function authFromUser($user) {
+            try {
+                // attempt to verify the credentials and create a token for the user
+                if (! $token = \JWTAuth::fromUser($user)) {
+                    return response()->json(['error' => 'invalid_credentials'], 401);
+                }
+            } catch (JWTException $e) {
+                // something went wrong whilst attempting to encode the token
+                return response()->json(['error' => 'could_not_create_token'], 500);
+            }
+
+            // all good so return the token
+            return response()->json(compact('token'));
+    }
 }
