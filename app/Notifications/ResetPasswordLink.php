e979195a6db5e1fa58f4bb24532097ceae7d3518
Petter <petter@maksimer.no>
2017-04-09 21:51:25 +0200

    Changed password reset text

--- app/Notifications/ResetPasswordLink.php
+++ app/Notifications/ResetPasswordLink.php
@@ -45,7 +45,7 @@ class ResetPasswordLink extends Notification
     {
 
         return (new MailMessage)
-            ->line('BOM! You are receiving this email because we received a password reset request for your account.')
+            ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', url('#/password/reset-form', ['token' => $this->token]))
             ->line('If you did not request a password reset, no further action is required.');
     }
