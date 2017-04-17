ed69e9d89dbd97ff4b3e363d34faab0bf009500a
Eivind Røe <eivindroe@gmail.com>
2017-04-17 09:42:18 +0200

    Improved footer content and layout

--- resources/assets/js/components/Footer.vue
+++ resources/assets/js/components/Footer.vue
@@ -3,34 +3,39 @@
         <div class="container">
             <div class="columns">
                 <div class="column">
-                    <p class="subtitle is-4">
-                        Spread the word and inspire others<br><small>- to share their favourite tent sites!</small></p>
-                    <p><small>We encourage you to bring this message further on, in hope of a growing database of
-                        tent sites throughout the world. We will promote tent sites through this instagram account on
-                        request, to spread the word and inspire to share.</small></p>
-                    <p><a href="https://instagram.com/mytentsite"
-                          title="Instagram - @mytentsite" target="_blank"><i class="fa fa-2x fa-instagram"></i>
-                    </a> <a href="https://facebook.com/mytentsite"
-                            title="Facebook - mytentsite" target="_blank"><i class="fa fa-2x fa-facebook-square"></i>
-                    </a></p>
+                    <h1 class="title">Spread the word and inspire others</h1>
+                    <h2 class="subtitle">- to share their favourite tent sites!</h2>
+                    <p>We encourage you to bring this message further on, in hope of a growing database of
+                        tent sites throughout the world. We will promote tent sites through our instagram account on
+                        request, to spread the word and inspire to share.</p>
+                    <br>
+                    <p>
+                        <a href="https://instagram.com/mytentsite" target="_blank" title="Instagram - @mytentsite"
+                           class="button is-light">
+                            <span class="icon is-medium"><i class="fa fa-instagram"></i></span>
+                            <span>Instagram</span>
+                        </a>
+                        <a href="https://facebook.com/mytentsite" target="_blank" title="Facebook - mytentsite"
+                           class="button is-light">
+                            <span class="icon is-medium"><i class="fa fa-facebook"></i></span>
+                            <span>Facebook</span>
+                        </a>
+                    </p>
                     <br>
                     <p><small>
-                        We use cookies to give you the best possible user experience.<br>
+                        We use cookies to give you the best possible user experience.
                         With continued use of our service, we assume that you agree to our use of cookies.</small>
                     </p>
                 </div>
                 <div class="column" v-if="beta">
-                    <p class="subtitle is-4">BETA <i class="fa fa-exclamation-circle fa-4x"></i></p>
-                    <p>We are still working on some features before we can publish this site, <br>
-                        and appreciate that you have joined us one the journey.</p>
-                    <p>Please submit both bug reports and feature ideas to us and we <br>
-                        will take it into consideration on further development.<br>
-                    </p>
-                    <p>
-                        <strong>
-                            You are first in line to be noticed when we are ready to launch this site :)
-                        </strong>
-                    </p>
+                    <h1 class="title">BETA</h1>
+                    <h2 class="subtitle">- work in progress</h2>
+                    <p>We are still working on some features before we can publish this site,
+                        and appreciate that you have joined us one the journey.<br><br>
+                        Please submit both bug reports and feature ideas to us through our
+                        <a href="https://facebook.com/mytentsite" target="_blank"
+                           title="Facebook - mytentsite">facebook page</a> and we
+                        will take it into consideration on further development.</p>
                 </div>
             </div>
         </div>
