b5d15f70138e2346e05a531bafaf2a3a2872b666
Petter <petter@maksimer.no>
2017-05-11 22:17:56 +0200

    Fixed reset password link

--- app/Notifications/ResetPasswordLink.php
+++ app/Notifications/ResetPasswordLink.php
@@ -46,7 +46,7 @@ class ResetPasswordLink extends Notification
 
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url('#/password/reset-form', ['token' => $this->token]))
+            ->action('Reset Password', env('APP_URL').'/#/password/reset-form?token='. $this->token)
             ->line('If you did not request a password reset, no further action is required.');
     }
 
