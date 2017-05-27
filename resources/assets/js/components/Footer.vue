a528ad854eb202c0174e0acc6fcf7894cf0bb462
Eivind Røe <eivindroe@gmail.com>
2017-05-27 12:42:50 +0200

    Added about and contact information in footer

--- resources/assets/js/components/Footer.vue
+++ resources/assets/js/components/Footer.vue
@@ -5,9 +5,9 @@
                 <div class="column is-half">
                     <h1 class="title is-5">Spread the word and inspire others</h1>
                     <h2 class="subtitle is-6">- to share their favourite tent sites!</h2>
-                    <p class="content is-small">We encourage you to bring this message further on, in hope of a growing database of
-                        tent sites throughout the world. We will promote tent sites through our instagram account on
-                        request, to spread the word and inspire to share.</p>
+                    <p class="content is-small">We encourage you to bring this message further on, in hope of a growing
+                        database of tent sites throughout the world. We will promote tent sites through our
+                        instagram account on request, to spread the word and inspire to share.</p>
                     <br>
                     <p>
                         <a href="https://instagram.com/mytentsite" target="_blank" title="Instagram - @mytentsite"
@@ -25,6 +25,21 @@
                     <p class="content is-small"><small>
                         We use cookies to give you the best possible user experience.
                         With continued use of our service, we assume that you agree to our use of cookies.</small>
+                    </p>
+                </div>
+                <div class="column is-half">
+                    <h1 class="title is-5">About</h1>
+                    <h2 class="subtitle is-6">- people behind and site info</h2>
+                    <p class="content is-small">
+                        The concept and solution <strong>mytentsite</strong> is founded, developed and maintained
+                        by Petter Alstad and Eivind Røe (Eipe).<br>
+                        We launched the first version in 2017 after months of planning and development and proudly
+                        present to you this collaborative tent collective!
+                    </p>
+                    <p class="content is-small">
+                        Feel free to contact us through or social profiles or directly on email
+                        mytentsite@gmail.com if you have any questions, ideas or bug reports.
+                        We really would appreciate your inputs!
                     </p>
                 </div>
                 <div class="column" v-if="beta">
