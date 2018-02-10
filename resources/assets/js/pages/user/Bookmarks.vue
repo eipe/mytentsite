2b9e297ba796d686176698e2278909e8dc6bc561
Eivind Røe <eivindroe@gmail.com>
2018-02-10 12:14:05 +0100

    Added view of bookmarks
    This make it possible for use to see tentsites that is bookmarked

--- resources/assets/js/pages/user/Bookmarks.vue
+++ resources/assets/js/pages/user/Bookmarks.vue
@@ -1,31 +1,83 @@
 <template>
     <div>
-        <section class="hero is-info is-fullheight">
-            <div class="hero-head">
-                <div class="has-text-centered">
-                    <br><br>
-                    <h1 class="title">Feature under development</h1>
-                    <transition enter-active-class="animated fadeIn">
-                        <h2 class="subtitle" v-if="subtitle">{{ subtitle }}</h2>
-                    </transition>
+        <div v-if="tentSites">
+            <section class="hero">
+                <div class="hero-body">
+                    <div class="container">
+                        <h2 class="title">Your bookmarks</h2>
+                        <button class="button" @click.prevent="loadTentSites"
+                                v-if="!isLoaded" v-bind:class="{ 'is-loading disabled' : isLoading }">Try again</button>
+                        <div class="columns is-multiline is-mobile">
+                            <template v-for="tentSite in bookmarkedTentSites">
+                                <photo class="column is-2" :id="tentSite.id"
+                                       :img_location="tentSite.img_location"
+                                       :thumbnail="tentSite.thumbnail"
+                                       :lat="tentSite.lat"
+                                       :lng="tentSite.lng"
+                                       :caption="tentSite.caption"
+                                       :reported_by="tentSite.reported_by"
+                                       :created_at="tentSite.created_at"
+                                       :bookmarks="tentSite.bookmarks"
+                                       :approved="tentSite.approved"
+                                       :showDetails="false"
+                                       :showControllers="false">
+                                </photo>
+                            </template>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 </template>
 <script>
+
+    import Photo from "../../components/Photo.vue";
+
     export default {
         name: "User-bookmarks",
         data() {
             return {
-                subtitle: ""
+                tentSites: [],
+                isLoaded: false,
+                isLoading: false,
+            }
+        },
+        methods: {
+            addTentSite(tentSite) {
+                this.tentSites.push(tentSite);
+            },
+            loadTentSites() {
+                let me = this;
+                me.tentSites = [];
+                me.isLoading = true;
+                Vue.axios.get("bookmarks").then(function success(success) {
+                    if(typeof success.data !== typeof undefined) {
+                        success.data.data.forEach(function (photo) {
+                            photo.thumbnail = '/storage/photos/tentsite_thumbnails/' + photo.img_location;
+                            me.addTentSite(photo);
+                        });
+                        me.isLoaded = true;
+                        me.isLoading = false;
+                    }
+                }).catch(function error(error) {
+                    me.isLoading = false;
+                    me.isLoaded = false;
+                    me.tentSites = [];
+                    me.$store.dispatch("displayError", "Could not load your bookmarks. <br>Please try again later");
+                });
+            }
+        },
+        computed: {
+            bookmarkedTentSites() {
+                return this.tentSites;
             }
         },
         created() {
-            let me = this;
-            setTimeout(function() {
-                me.subtitle = "Stay tuned - it won't take long time!";
-            }, 1000);
+           this.loadTentSites();
+        },
+        components: {
+            Photo
         }
     }
 </script>
