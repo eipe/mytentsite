3a2522c5ae7a6a82ae0396f14477116fc0251544
Eivind Røe <eivindroe@gmail.com>
2018-05-03 13:28:02 +0200

    Added tag filter on explore wall
    This make it possible for the user to filter tent sites based on tags

--- resources/assets/js/pages/Explore.vue
+++ resources/assets/js/pages/Explore.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <section class="section container">
+            <div class="tags">
+                <span v-for="tag in tags"
+                      :data-id="tag.id"
+                      class="tag is-clickable"
+                      @click="toggleTagFilter(tag)"
+                      v-bind:class="{'is-success' : isTagFilterActive(tag)}">
+                    {{ tag.name }}
+                </span>
+            </div>
             <div class="columns is-multiline is-mobile has-normal-margin">
                 <div v-for="tentSite in tentSites" class="column is-one-third"
                        :key="tentSite.id" @click="openGallery(tentSite)">
@@ -33,14 +42,42 @@
                 hasMore: true,
                 firstPhotoId: null,
                 lastPhotoId: null,
+                tags: [],
+                filter: {
+                    tags: []
+                }
             }
         },
         created() {
             if(typeof this.tentSites === typeof undefined || this.tentSites.length === 0) {
                 this.loadMoreTentSites();
             }
+
+            let me = this;
+
+            Vue.axios.get('tags').then((response) => {
+                response.data.data.forEach((tagData) => {
+                    let tag = {
+                        id: tagData.id,
+                        name: tagData.name
+                    };
+                    me.tags.push(tag);
+                });
+            });
         },
         methods: {
+            toggleTagFilter(tag) {
+                let tagIndex = this.filter.tags.indexOf(tag);
+
+                if(tagIndex > -1) {
+                    this.filter.tags.splice(tagIndex, 1);
+                } else {
+                    this.filter.tags.push(tag);
+                }
+            },
+            isTagFilterActive(tag) {
+                return (this.filter.tags.indexOf(tag) > -1);
+            },
             openGallery(tentSite) {
                 this.$refs.gallery.openGallery(tentSite);
             },
@@ -87,12 +124,28 @@
         },
         computed: {
             tentSites() {
-                let me = this;
-                return me.tentSiteIds.map(function (id) {
+                let me = this,
+                    tentSites = [],
+                    filterByTags = (me.filter.tags.length > 0);
+
+                me.tentSiteIds.forEach((id) => {
                     if(me.$store.state.tentSites.hasOwnProperty(id)) {
-                        return me.$store.state.tentSites[id];
+                        let tentSite = me.$store.state.tentSites[id];
+                        if(filterByTags) {
+                            let matchingFilters = me.filter.tags.filter((tag) => {
+                                if(tentSite.tags.indexOf(tag.name) > -1) {
+                                    return tag;
+                                }
+                            });
+                            if(matchingFilters.length > 0) {
+                                tentSites.push(tentSite);
+                            }
+                        } else {
+                            tentSites.push(tentSite);
+                        }
                     }
                 });
+                return tentSites;
             }
         },
         components: {
